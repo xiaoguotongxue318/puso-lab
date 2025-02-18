46a18034a068d630d4ad57a02f790625e9629ab4
xiaoguotongxue318 <1023056889@qq.com>
2025-02-18 13:42:46 +0800

    modified:   docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 project = 'puso-lab'
 copyright = '2025, puso'
